adf965d6125093d088f368ce3a620743da3d92e0
shiki-tak <shiki.tak@gmail.com>
2020-06-01 22:43:03 +0900

    add unary plus and minus

--- 9cc.c
+++ 9cc.c
@@ -172,6 +172,7 @@ Node *new_node_num(int val)
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 // expr = mul ("+" mul | "-" mul)*
@@ -189,19 +190,30 @@ Node *expr()
     }
 }
 
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
 Node *mul()
 {
-    Node *node = primary();
+    Node *node = unary();
 
     for (;;) {
         if (consume('*'))
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         else
             return node;
     }
+}
+
+// unary = ("+" | "-")? unary
+//       | primary
+Node *unary()
+{
+    if (consume('+'))
+        return unary();
+    if (consume('-'))
+        return new_node(ND_SUB, new_node_num(0), unary());
+    return primary();
 }
 
 // primary = "(" expr ")" | num
